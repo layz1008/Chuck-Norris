b0f5578d495238efdb2f96f87f3ecb6d8d132642
layz1008 <zlay1008@gmail.com>
2023-01-11 10:09:25 -0500

    Started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// import burger UI component
+import Burger from './Burger';
 
 function App() {
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://api.icndb.com/jokes/random')
+      .then(response => setJoke(response.data.value.joke));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Burger />
+      <div>
+        <p>{joke}</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
